1c0139f738228153c27feeabb02149454d1d1c75
62105077 <71056617+62105077@users.noreply.github.com>
2021-08-13 15:25:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <h1><center>Nirawan Ausakan 62105077</center></h1><br>
 <center><iframe src= "https://thingspeak.com/channels/1458420/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="300"></iframe>
 <iframe src="https://thingspeak.com/channels/1458420/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="300"></iframe>
-<iframe src="https://thingspeak.com/channels/1458420/maps/channel_show"width="450" height="200"></iframe></center>
+<iframe src="https://thingspeak.com/channels/1458420/maps/channel_show"width="450" height="290"></iframe></center>
