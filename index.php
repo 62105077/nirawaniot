bd32192421b2d7acc4b09ce11e1114d9d7e26d35
62105077 <71056617+62105077@users.noreply.github.com>
2021-08-13 14:16:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
-Nirawan Ausakan
+<h>Nirawan Ausakan</h>
+<iframe src="https://thingspeak.com/channels/1458420/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15">
