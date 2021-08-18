5e240c605196cdbf8d3d876b4cd5b3a71ab7eb77
62105077 <71056617+62105077@users.noreply.github.com>
2021-08-18 17:28:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
   
   <center><h1 style="border: 2px solid Violet;">Nirawan Ausakan 62105077</h1> </center><br>
 
-<center><iframe src= "https://thingspeak.com/channels/1458420/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="260"></iframe><br>
+<center><iframe src= "https://thingspeak.com/channels/1458420/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="260"></iframe><br><br>
   <?php
         $api_url = 'https://api.thingspeak.com/channels/1458420/fields/1.json?results=1';
         $json_data = file_get_contents($api_url);
@@ -19,7 +19,7 @@
         ?>
      <br>
     
-<iframe src="https://thingspeak.com/channels/1458420/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="260"></iframe>
+<iframe src="https://thingspeak.com/channels/1458420/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="260"></iframe><br><br>
    <?php
         $api_url = 'https://api.thingspeak.com/channels/1458420/fields/2.json?results=1';
         $json_data = file_get_contents($api_url);
