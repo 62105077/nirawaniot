8a69839d4bf817011e9df91bc7e36aea8e7528cf
62105077 <71056617+62105077@users.noreply.github.com>
2021-08-18 17:20:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <center><iframe src= "https://thingspeak.com/channels/1458420/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="260"></iframe>
   <?php
-        $api_url = 'https://api.thingspeak.com/channels/1458420/fields/1.json?results=2';
+        $api_url = 'https://api.thingspeak.com/channels/1458420/fields/1.json?results=1';
         $json_data = file_get_contents($api_url);
         $response_data = json_decode($json_data);
         $user_data = $response_data->feeds;
@@ -16,11 +16,11 @@
             echo " %";
          }
         ?>
-     </p><br>
+     <br>
     
 <iframe src="https://thingspeak.com/channels/1458420/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="260"></iframe>
    <?php
-        $api_url = 'https://api.thingspeak.com/channels/1458420/fields/2.json?results=2';
+        $api_url = 'https://api.thingspeak.com/channels/1458420/fields/2.json?results=1';
         $json_data = file_get_contents($api_url);
         $response_data = json_decode($json_data);
         $user_data = $response_data->feeds;
@@ -30,7 +30,7 @@
              echo " C"; 
          }
         ?><br>
-<iframe src="https://thingspeak.com/channels/1458420/maps/channel_show"width="450" height="260"></iframe></center>
+<center><iframe src="https://thingspeak.com/channels/1458420/maps/channel_show"width="450" height="260"></iframe></center>
   </body>
 </html>
 
