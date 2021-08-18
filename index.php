1ee43e098eaa4e50b4b6be092ded476f2042086e
62105077 <71056617+62105077@users.noreply.github.com>
2021-08-18 17:15:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head>
 </head>
  <body>
-  <h1 style="border: 2px solid Violet;">Nirawan Ausakan</h1><br>
+  <center><h1 style="border: 2px solid Violet;">Nirawan Ausakan</h1> <center><br>
 
 <center><iframe src= "https://thingspeak.com/channels/1458420/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15"width="450" height="260"></iframe>
   <?php
